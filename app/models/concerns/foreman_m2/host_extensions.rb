cfb378b277fccb2bc43610d6dc65fee7d98d8043
Ian Ballou <ianballou67@gmail.com>
2018-08-22 16:05:32 -0400

    starting to add host hybrid changes to the host extensions

--- app/models/concerns/foreman_m2/host_extensions.rb
+++ app/models/concerns/foreman_m2/host_extensions.rb
@@ -7,6 +7,9 @@ module ForemanM2
       # execute callbacks
       #before_destroy :destroy_disk, :if => compute_resource.type == "ForemanM2::M2"
       before_destroy :destroy_disk
+      validates :root_pass, :length => {:minimum => 8, :message => _('should be 8 characters or more')},
+                            :presence => {:message => N_('should not be blank - consider setting a global or host group default')},
+                            :if => Proc.new { |host| host.managed && (host.pxe_build? || host.hybrid_build?) && build? }
     end
 
     # create or overwrite instance methods...
@@ -18,7 +21,58 @@ module ForemanM2
     end
 		
     class ::Host::Managed::Jail < Safemode::Jail
-      allow :iscsi_target
+      allow :iscsi_target, :hybrid_build?, :image_name
+    end
+
+    def self.provision_methods
+      {
+        'build' => N_('Network Based'),
+        'image' => N_('Image Based'),
+        'hybrid' => N_('Image Based Network')
+      }.merge(registered_provision_methods)
+    end
+
+    def can_be_built?
+      managed? && SETTINGS[:unattended] && (pxe_build? || hybrid_build?) && !build?
+    end
+
+    def inherited_attributes
+      inherited_attrs = %w{domain_id}
+      if SETTINGS[:unattended]
+        inherited_attrs.concat(%w{operatingsystem_id architecture_id compute_resource_id})
+        inherited_attrs << "subnet_id" unless compute_provides?(:ip)
+        inherited_attrs << "subnet6_id" unless compute_provides?(:ip6)
+        inherited_attrs.concat(%w{medium_id ptable_id pxe_loader}) if (pxe_build? || hybrid_build?)
+      end
+      inherited_attrs
+    end
+
+    def hybrid_build?
+      self.provision_method == 'hybrid'
+    end
+
+    def ensure_associations
+      status = true
+      if SETTINGS[:unattended] && managed? && os && (pxe_build? || hybrid_build?)
+        %w{ptable medium architecture}.each do |e|
+          value = self.send(e.to_sym)
+          next if value.blank?
+          unless os.send(e.pluralize.to_sym).include?(value)
+            errors.add("#{e}_id".to_sym, _("%{value} does not belong to %{os} operating system") % { :value => value, :os => os })
+            status = false
+          end
+        end
+      end
+
+      if environment
+        puppetclasses.select("puppetclasses.id,puppetclasses.name").distinct.each do |e|
+          unless environment.puppetclasses.map(&:id).include?(e.id)
+            errors.add(:puppetclasses, _("%{e} does not belong to the %{environment} environment") % { :e => e, :environment => environment })
+            status = false
+          end
+        end
+      end
+      status
     end
 
     def iscsi_target
@@ -33,7 +87,6 @@ module ForemanM2
 			proxy = ::ProxyAPI::M2.new(url: SmartProxy.with_features('M2').first.url)
       proxy.delete_iscsi_target(:disk => name)
     end
-
 
     module ClassMethods
       # create or overwrite class methods...
