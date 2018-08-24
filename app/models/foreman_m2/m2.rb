cfa798d1491cf3075dcf42682bbac7592b3c1227
Ian Ballou <ianballou67@gmail.com>
2018-08-24 19:02:26 -0400

    now selecting m2 smart proxy by url

--- app/models/foreman_m2/m2.rb
+++ app/models/foreman_m2/m2.rb
@@ -1,24 +1,23 @@
 module ForemanM2
-	class M2 < ComputeResource
+  class M2 < ComputeResource
+    def self.model_name
+      ComputeResource.model_name
+    end
 
-		def self.model_name
-			ComputeResource.model_name
-		end
+    def capabilities
+      %i[build image hybrid]
+    end
 
-		def capabilities
-			[:build, :image, :hybrid]
-		end
+    def available_images
+      proxy = SmartProxy.find_by name: "proxy_m2"
+      proxyAPI = ::ProxyAPI::M2.new(url: proxy.url)
+      # proxy = ::ProxyAPI::M2.new(url: url)
+      proxyAPI.get_images([])
+    end
 
-		def available_images
-			proxy = ::ProxyAPI::M2.new(url: SmartProxy.with_features('M2').first.url)
-			#proxy = ::ProxyAPI::M2.new(url: url)
-			proxy.get_images([])
-		end
-
-		def client
-			# XXX TOTAL SPOOF
-			#@client = Fog::Compute.new(:provider => "Libvirt", :libvirt_uri => 'qemu:///system')
-		end
-
-	end
+    def client
+      # XXX TOTAL SPOOF
+      # @client = Fog::Compute.new(:provider => "Libvirt", :libvirt_uri => 'qemu:///system')
+    end
+  end
 end
