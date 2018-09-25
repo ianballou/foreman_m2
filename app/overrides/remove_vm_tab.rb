6a2e632622b29b3e107366075a9d1713f631f1ec
Ian Ballou <ianballou67@gmail.com>
2018-09-25 17:02:28 -0400

    VM tab no longer shown during host creation

---
+++ app/overrides/remove_vm_tab.rb
@@ -0,0 +1,4 @@
+Deface::Override.new(:virtual_path => 'hosts/_form',
+                     :name => 'remove_vm_tab',
+                     :remove => 'li#compute_resource_tab')
+                       #'erb[loud]:contains("select_f"):contains(":ptable_id")')
